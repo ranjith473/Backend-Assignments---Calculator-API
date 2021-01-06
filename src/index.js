d5aa9cb9e9935eb25f81863b6641af0a86db3050
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-01-06 22:38:49 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -91,6 +91,89 @@ app.post("/sub", (req, res) => {
   res.send(obj1);
 });
 
+app.post("/multiply", (req, res) => {
+  const obj1 = {
+    status: "",
+    message: "",
+    result: 0
+  };
+  if (
+    typeof req.body.num1 === "string" ||
+    req.body.num1 instanceof String ||
+    typeof req.body.num2 === "string" ||
+    req.body.num2 instanceof String
+  ) {
+    obj1.status = "error";
+    obj1.message = "Invalid data types";
+  } else {
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+      obj1.status = "error";
+      obj1.message = "Invalid data type";
+    } else {
+      const result1 = num1 * num2;
+      if (num1 < -1000000 || num2 < -1000000 || result1 < -1000000) {
+        obj1.status = "error";
+        obj1.message = "Underflow";
+      } else if (num1 > 1000000 || num2 > 1000000 || result1 > 1000000) {
+        obj1.status = "error";
+        obj1.message = "Overflow";
+      } else {
+        obj1.status = "success";
+        obj1.message = "The product of given numbers";
+        obj1.result = result1;
+      }
+    }
+  }
+
+  res.send(obj1);
+});
+
+app.post("/divide", (req, res) => {
+  const obj1 = {
+    status: "",
+    message: "",
+    result: 0
+  };
+  if (
+    typeof req.body.num1 === "string" ||
+    req.body.num1 instanceof String ||
+    typeof req.body.num2 === "string" ||
+    req.body.num2 instanceof String
+  ) {
+    obj1.status = "error";
+    obj1.message = "Invalid data types";
+  } else {
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+      obj1.status = "error";
+      obj1.message = "Invalid data type";
+    } else if (num2 === 0) {
+      obj1.status = "error";
+      obj1.message = "Cannot divide by zero";
+    } else {
+      const result1 = num1 / num2;
+      if (num1 < -1000000 || num2 < -1000000 || result1 < -1000000) {
+        obj1.status = "error";
+        obj1.message = "Underflow";
+      } else if (num1 > 1000000 || num2 > 1000000 || result1 > 1000000) {
+        obj1.status = "error";
+        obj1.message = "Overflow";
+      } else {
+        obj1.status = "success";
+        obj1.message = "The division of given numbers";
+        obj1.result = result1;
+      }
+    }
+  }
+
+  res.send(obj1);
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
